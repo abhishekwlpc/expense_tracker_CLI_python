38f180f06ef01ba3f5b732f9c8a61f4707e9cf8d
Abhishek Pramuditha <abhishekwlpc@gmail.com>
2026-08-13 09:02:12 +0530

    Enhance expense management with input validation and error handling
    - Introduced custom exceptions for invalid text and amount inputs.
    - Improved load_expenses function with detailed error reporting.
    - Updated add_expense function to validate title and amount.
    - Enhanced delete functionality to handle empty expense lists.
    - Added new expense entries to JSON for testing.

--- user_action_methods.py
+++ user_action_methods.py
@@ -1,54 +1,49 @@
 import json
 import uuid
 import datetime
+import custom_exceptions
+
 
 def load_expenses():
     expense_list = []
-    try:
+    try:  
         # Get the current list from JSON and put it into expense_list
         with open("expenses_details/full_details.json", "r") as file1:
             expense_list = json.load(file1)
         return expense_list
 
     except FileNotFoundError:
-         return expense_list
+        return expense_list
     
     except json.JSONDecodeError as e:
-         print("Invalid JSON syntax: , Please check the json file and fix the syntax: ", e)
-
-
-    except Exception as e:
-        print("Some Error Occured: ", e)
-        return expense_list
+        print(f"Failed to parse JSON. Error: {e.msg}")
+        print(f"Error occured at line {e.lineno}, column {e.colno}")
+        pass
 
 def save_expenses(expenses):
-    try:
         with open("expenses_details/full_details.json", "w") as file:
             json.dump(expenses, file, indent=4)
                 
-    except FileNotFoundError:
-        open("expenses_details/full_details.json", "x")
-        json.dump(expenses, "expenses_details/full_details.json", indent=4)
 
 
 def print_expense_list():
-    try:
-        expenses = load_expenses()
-        
-        # Get the current list from JSON and put it into expense_list
-        for i in range(0,len(expenses)):
-            print(f"Id: {expenses[i]["Id"]} | Name: {expenses[i]["Title"]} | Date: {expenses[i]["Date"]} | Amount You spend for this: {expenses[i]["Amount"]}")
 
-    except Exception as e:
-        print("Some error occured" ,e)
-      
+    expenses = load_expenses()
+    
+    # Get the current list from JSON and put it into expense_list
+    for item in expenses:
+        print(item)
 
 def add_expense():
 
     try:
         print("===============================ADD EXPENSES===================================")
         # Get inputs from the CLI
+
         title = str(input("Please enter the expense name/title: "))
+        if(title.isspace() or title == ""):
+            raise custom_exceptions.InvalidTextError("Title can not contain spaces or can not be empty")
+        
         month = int(input("Enter the month as number(1-12): "))
         date1 = int(input("Enter the date(1-31): "))
         year = int(input("Enter the year: "))
@@ -56,6 +51,10 @@ def add_expense():
         date_ = str(datetime.date(year,month,date1))
         
         amount = float(input("Enter the amount you spent: "))
+
+        if(amount <= 0):
+            raise custom_exceptions.InvalidAmountError("Amount can not be 0 or negative!")
+        
         ide = str(uuid.uuid4())
 
 
@@ -73,10 +72,14 @@ def add_expense():
 
 
         print("=================================ADDED SUCCESSFULLY=============================")
+
+    except custom_exceptions.InvalidTextError as e:
+        print("Caught an error: ", e)
+    except custom_exceptions.InvalidAmountError as e:
+        print("Caught an error: ", e)
     except ValueError as e:
         print("Your typed output is not correct. Please try again: ",e)
-    except Exception as e:
-        print("Some error Occured: ", e)
+
 
 def view_expenses_list():
     print("===============================TOTAL EXPENSES===================================\n")
@@ -88,53 +91,53 @@ def get_total_spent_amount():
     print("=========================TOTAL AMOUNT SPEND SO FAR==============================")
     expense_list = load_expenses()
     total_amount = 0.0
-    for j in range(0,len(expense_list)):
-        total_amount += expense_list[j]['Amount']
-
+    for item in expense_list:
+        total_amount += item['Amount']
 
     print(f"Total Amount you spend so far is: {total_amount}")
 
     print("================================================================================")
 
 def delete_expense_from_list():
-    try:
-        print("===============================DELETE EXPENSES===================================\n")
-        
-        # show list for user to see id and other details
-        print_expense_list()
 
-        deleted_item_id = str(input("\nPlease enter id of item that you want to delete: "))
-        expense_lists = load_expenses()
-        is_entered_id_exist = False
-        
-        for i in range(0,len(expense_lists)):
-            if(str(expense_lists[i]['Id']) == deleted_item_id):
-                expense_lists.pop(i)
-                is_entered_id_exist = True
-                break
-        save_expenses(expense_lists)
+        print("===============================DELETE EXPENSES===================================\n")
+        expense_list = load_expenses()
+        if(len(expense_list) != 0):
+
+        # show list for user to see id and other details
+            print_expense_list()
     
-        if(is_entered_id_exist):
-            print("=================================DELETED SUCCESSFULLY=============================")
+            deleted_item_id = str(input("\nPlease enter id of item that you want to delete: "))
+            is_entered_id_exist = False              
+            
+            for index, item in enumerate(expense_list):
+                if(str(item['Id']) == deleted_item_id):
+                    expense_list.pop(index)
+                    is_entered_id_exist = True
+                    break
+        
+            if(is_entered_id_exist):
+                save_expenses(expense_list)
+                print("=================================DELETED SUCCESSFULLY=============================")
+            else:
+                print("Your Entered Item is not on the list")
         else:
-            print("Your Entered Item is not on the list")
-    except Exception as e:
-         print("Error Occured: ", e)
+            print("No expenses available to delete!")
+
 
 def delete_all_expenses():
     print("===============================DELETE ALL EXPENSES===================================\n")
-    try: 
-        confirm = input("Please type \"confirm\" to delete all current expenses from your list: ")
-    except ValueError:
-         print("Your typed output is invalid!")
-    except Exception as e:
-         print("Error Occured: ", e)
-    if(confirm == "confirm"):
-        expense_list = load_expenses()
-        expense_list.clear()
-
-        save_expenses(expense_list)
-
-        print("=================================DELETED ALL EXPENSES SUCCESSFULLY=============================\n")
+    expense_list = load_expenses()
+    if(len(expense_list) == 0):
+        print("No expenses available to delete!")
     else:
-         print("You didn't confirm from your side to delete all expenses. Please try again\n")
+        confirm = input("Please type \"confirm\" to delete all current expenses from your list: ")
+        
+        if(confirm == "confirm"):
+            expense_list.clear()
+
+            save_expenses(expense_list)
+
+            print("=================================DELETED ALL EXPENSES SUCCESSFULLY=============================\n")
+        else:
+            print("You didn't confirm from your side to delete all expenses. Please try again\n")
